8157c87ea347fe844b1544a4d32f08bc7b3b2b15
darshan <darshan@melkote.com>
2020-07-08 09:41:12 +0530

    split func listner int func accsept ans func listner

--- vperf server.go
+++ vperf server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 )
 
 const Port = 2222
@@ -11,25 +11,27 @@ type server struct {
 	conn *net.TCPConn
 }
 
-func listuin () (*server , error) {
+func listen () (*net.TCPListener , error) {
 	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%v", Port))
 	if err != nil {
 		return nil, err
 	}
 
-	listner , err := net.ListenTCP("tcp" , addr)
+	listner, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		return nil , err
+		return nil, err
 	}
 
+	return listner , nil
+}
+
+func accsept (listner *net.TCPListener) (*server , error){
 	conn , err := listner.AcceptTCP()
 	if err != nil {
-		listner.Close()
+		_ = listner.Close()
 		return nil , err
 	}
 
 	serv := server{conn: conn}
 	return &serv , nil
 }
-
-
